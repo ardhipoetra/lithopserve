1402c6816384226e0ef26d6810e1437964c66e87
ardhipoetra <me@ardhipoetra.web.id>
2024-09-19 11:20:22 +0200

    Add Scontain changes

--- lithops/serverless/backends/k8s/k8s.py
+++ lithops/serverless/backends/k8s/k8s.py
@@ -492,7 +492,9 @@ class KubernetesBackend:
         master_res['metadata']['name'] = self.master_name
         master_res['metadata']['namespace'] = self.namespace
         master_res['metadata']['labels']['version'] = 'lithops_v' + __version__
-        master_res['metadata']['labels']['user'] = self.user
+        # master_res['metadata']['labels']['user'] = self.user
+        # mig 14apr2024 - Patch by Miguel @ SCONTAIN. Added '.replace('@', '--')'
+        master_res['metadata']['labels']['user'] = self.user.replace('@', '--')
         master_res['spec']['template']['spec']['containers'][0]['resources'] = config.MASTER_CONFIG_RESOURCES
 
         container = master_res['spec']['template']['spec']['containers'][0]
@@ -610,7 +612,9 @@ class KubernetesBackend:
         job_res['metadata']['name'] = activation_id
         job_res['metadata']['namespace'] = self.namespace
         job_res['metadata']['labels']['version'] = 'lithops_v' + __version__
-        job_res['metadata']['labels']['user'] = self.user
+        # job_res['metadata']['labels']['user'] = self.user
+        # mig 14apr2024 - Patch by Miguel @ SCONTAIN. Added '.replace('@', '--')'
+        job_res['metadata']['labels']['user'] = self.user.replace('@', '--')
 
         job_res['spec']['activeDeadlineSeconds'] = self.k8s_config['runtime_timeout']
         job_res['spec']['parallelism'] = total_workers
@@ -659,7 +663,9 @@ class KubernetesBackend:
         job_res['metadata']['name'] = meta_job_name
         job_res['metadata']['namespace'] = self.namespace
         job_res['metadata']['labels']['version'] = 'lithops_v' + __version__
-        job_res['metadata']['labels']['user'] = self.user
+        # job_res['metadata']['labels']['user'] = self.user
+        # mig 14apr2024 - Patch by Miguel @ SCONTAIN. Added '.replace('@', '--')'
+        job_res['metadata']['labels']['user'] = self.user.replace('@', '--')
 
         container = job_res['spec']['template']['spec']['containers'][0]
         container['image'] = docker_image_name
