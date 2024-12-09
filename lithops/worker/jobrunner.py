4a1eba0b8a4c174f4e60ec484903a8170d38ce79
ardhipoetra <me@ardhipoetra.web.id>
2024-12-09 13:26:52 +0100

    [DRAFT] various hacks

--- lithops/worker/jobrunner.py
+++ lithops/worker/jobrunner.py
@@ -68,7 +68,7 @@ class JobRunner:
         self.jobrunner_conn = jobrunner_conn
         self.internal_storage = internal_storage
         self.lithops_config = job.config
-
+        self.alive = True
         self.output_key = create_output_key(job.executor_id, job.job_id, job.call_id)
 
         # Setup stats class
@@ -78,6 +78,9 @@ class JobRunner:
         prom_enabled = self.lithops_config['lithops'].get('telemetry')
         prom_config = self.lithops_config.get('prometheus', {})
         self.prometheus = PrometheusExporter(prom_enabled, prom_config)
+
+    def is_alive(self):
+        return self.alive
 
     def _fill_optional_args(self, function, data):
         """
@@ -313,3 +316,4 @@ class JobRunner:
                 self.stats.write("worker_result_upload_time", round(output_upload_end_tstamp - output_upload_start_tstamp, 8))
             self.jobrunner_conn.send("Finished")
             logger.info("Process finished")
+            self.alive = False
