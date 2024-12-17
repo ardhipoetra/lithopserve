92905b94f6d62a4044cdc2f6c67eca65161a4b4a
Josep <josep.calero@urv.cat>
2024-12-17 11:37:38 +0000

    Include extension in delete_runtime for K8s

--- lithops/serverless/backends/k8s/k8s.py
+++ lithops/serverless/backends/k8s/k8s.py
@@ -239,14 +239,25 @@ class KubernetesBackend:
         Deletes a runtime
         """
         job_name_pattern = self._format_job_name(docker_image_name, memory, version)
+
         docker_path = utils.get_docker_path()
         docker_user = self.k8s_config.get("docker_user")
         docker_password = self.k8s_config.get("docker_password")
         docker_server = self.k8s_config.get("docker_server")
+
         if docker_user and docker_password:
             logger.debug('Container registry credentials found in config. Logging in into the registry')
             cmd = f'{docker_path} login -u {docker_user} --password-stdin {docker_server}'
             utils.run_command(cmd, input=docker_password)
+
+        # Remove tag from docker_image_name
+        if ':' in docker_image_name:
+            base_image_name = docker_image_name.split(':')[0]
+            logger.debug(f'Removed tag from Docker image name. Base name: {base_image_name}')
+        else:
+            base_image_name = docker_image_name
+
+
         logger.info(f'Deleting Docker image {docker_image_name} in remote')
         cmd = f'{docker_path} image rm {docker_image_name}'
         try:
@@ -254,6 +265,16 @@ class KubernetesBackend:
             logger.info(f'Removed Docker image {docker_image_name} in remote')
         except Exception as e:
             logger.error(f"Failed to delete image: {docker_image_name} in remote")
+
+        logger.info(f'Deleting extended Docker image {docker_image_name}:ext in remote')
+        cmd = f'{docker_path} image rm {docker_image_name}:ext'
+        try:
+            utils.run_command(cmd)
+            logger.info(f'Removed extended Docker image {docker_image_name}:ext in remote')
+        except Exception as e:
+            logger.error(f"Failed to delete extended image: {docker_image_name}:ext in remote")
+
+
         logger.info(f"Deleting runtime: {docker_image_name} locally")
         cmd = f'{docker_path} rmi {docker_image_name}'
         try:
@@ -261,6 +282,15 @@ class KubernetesBackend:
             logger.info(f'Removed Docker image {docker_image_name} locally')
         except Exception as e:
             logger.error(f"Failed to delete image: {docker_image_name} locally")
+
+        logger.info(f"Deleting extended runtime: {docker_image_name}:ext locally")
+        cmd = f'{docker_path} rmi {docker_image_name}:ext'
+        try:
+            utils.run_command(cmd)
+            logger.info(f'Removed extended Docker image {docker_image_name}:ext locally')
+        except Exception as e:
+            logger.error(f"Failed to delete extended image: {docker_image_name}:ext locally")
+
         # Delete the Kubernetes Deployment
         try:
             logger.debug(f"Deleting job: {job_name_pattern}")
@@ -271,6 +301,7 @@ class KubernetesBackend:
             )
         except ApiException as e:
             logger.error(f"Failed to delete job: {job_name_pattern}. Reason: {str(e)}")
+
         # 2. Delete the container registry secret
         try:
             logger.debug("Deleting container registry secret")
@@ -278,6 +309,7 @@ class KubernetesBackend:
         except ApiException as e:
             if e.status != 404:
                 logger.error(f"Failed to delete secret: lithops-regcred. Reason: {str(e)}")
+
 
     def clean(self, all=False):
         """
